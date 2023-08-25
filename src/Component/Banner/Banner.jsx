821eddaf7e406e44355a0363db160ac73ff15206
Sadiqur199 <sadiqurrhaman199@gmail.com>
2023-08-26 02:42:36 +0600

    working the banner section and added banner image

--- src/Component/Banner/Banner.jsx
+++ src/Component/Banner/Banner.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React from "react";
+import image1 from '../../images/life@bsf-header-img.png'
 
 const Banner = () => {
   return (
-    <div>
-      <h1>this is banner</h1>
-    </div>
+    <>
+      <div className="hero min-h-screen bg-gray-300">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img
+            src={image1}
+            className=" shadow-3xl"
+          />
+          <div>
+            <h1 className="text-5xl font-bold">Box Office News!</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
